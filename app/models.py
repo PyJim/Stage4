8928d892561541937fae7ddc06dff30d1e59c4fb
Jimmy Essel <jimmykodwoessel@gmail.com>
2024-07-22 12:22:08 +0000

    database schema update

--- app/models.py
+++ app/models.py
@@ -4,8 +4,8 @@ from .database import Base
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    password_hash = Column(String)
-    name = Column(String)
+    username = Column(String(length=50), unique=True, index=True)
+    email = Column(String(length=100), unique=True, index=True)
+    password_hash = Column(String(length=255))
+    name = Column(String(length=100))
     preferences = Column(JSON)
